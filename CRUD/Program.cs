0a25a8b29a58a536ef997436fe3fb71d499aa814
Fawad Idrees <fawad.idrees@infinios.com>
2023-07-18 19:50:55 +0300

    fff

--- CRUD/Program.cs
+++ CRUD/Program.cs
@@ -45,11 +45,10 @@ builder.Services.AddSwaggerGen();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-//if (app.Environment.IsDevelopment())
-//{
+
     app.UseSwagger();
     app.UseSwaggerUI();
-//}
+
 app.UseAuthentication();
 app.UseAuthorization();
 
